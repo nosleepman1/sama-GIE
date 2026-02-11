e3262edb74f20bec01cf8110c66a64e1e35252bf
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-02-11 00:05:26 +0000

    add event feature for auth workflow

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['member','admin', 'director'])->default('member');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
